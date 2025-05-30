0211306bab85eeeaf17df99cc67ffa2340ce78c2
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2025-05-30 10:38:08 +0700

    add api

---
+++ src/components/movielist.svelte
@@ -0,0 +1,28 @@
+<script>
+  let movies = [];
+
+  const apiKey = '8f1aed9577242f589e9228998ae9be49';
+
+  const fetchMovies = async () => {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`);
+    const data = await res.json();
+    movies = data.results;
+  };
+
+  fetchMovies();
+</script>
+
+<h1>🎬 หนังยอดนิยม</h1>
+
+{#if movies.length > 0}
+  <div style="display: grid; grid-template-columns: repeat(auto-fit, minmax(150px, 1fr)); gap: 1rem;">
+    {#each movies as movie}
+      <div>
+        <img src={"https://image.tmdb.org/t/p/w200" + movie.poster_path} alt={movie.title} />
+        <p>{movie.title}</p>
+      </div>
+    {/each}
+  </div>
+{:else}
+  <p>กำลังโหลด...</p>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
+<script>
+    import Movielist from '../components/movielist.svelte';
+</script>
 <h1 class="text-3xl font-bold underline">
   Hello world!
 </h1>
+<Movielist />
 <style lang="postcss">
   @reference "tailwindcss";
   :global(html) {
